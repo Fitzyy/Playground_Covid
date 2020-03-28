4b332a4a242a25f246c614cfe600618333c29835
Nathan Good <goodlnath@gmail.com>
2020-03-28 21:26:04 +0000

    Initial commit

---
+++ app/src/main/java/com/tothemoon/playground2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tothemoon.playground2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
